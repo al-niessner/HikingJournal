2dbf4a00b335fd3d37425ebb93488886ba8b5a70
Al Niessner <Al.Niessner@gmx.net>
2015-03-08 21:42:22 -0700

    added a favion that I loaded from findicons.com with a freeware license

--- hj/fe/root.py
+++ hj/fe/root.py
@@ -1,5 +1,6 @@
 '''Root of the hiking journal'''
 
+import os
 import twisted.web.resource
 import twisted.web.server
 
@@ -23,10 +24,21 @@ class FrontPage(twisted.web.resource.Resource):
 '''
     pass
 
+class FavIcon(twisted.web.resource.Resource):
+    isLeaf = True
+    def render_GET (self, request):
+        data = b''
+        with open (os.path.abspath (os.path.join (os.path.dirname (__file__),
+                                                  'resources/favicon.ico')),
+                   'rb') as f: data = f.read()
+        return data
+    pass
+
 def run (port : int) -> None:
     '''Start up the twisted engine as the front-end'''
     root = Root()
     root.putChild (b'', FrontPage())
+    root.putChild (b'favicon.ico', FavIcon())
     factory = twisted.web.server.Site(root)
     twisted.internet.reactor.listenTCP (port, factory)
     twisted.internet.reactor.run()
