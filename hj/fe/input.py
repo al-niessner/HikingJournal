849a73cbd3c490db9fead2252fc4e04b218a1cea
Al Niessner <Al.Niessner@gmx.net>
2015-07-02 15:39:29 -0700

    scan is working and so is moving/copying of the files about but the two items that need attention are the blocking screen when busy and the opening/closing of the device

--- hj/fe/input.py
+++ hj/fe/input.py
@@ -4,14 +4,33 @@
 from hj.fe import fapp
 
 import flask
+import hj.config
 import hj.fe
 import hj.fe.forms
 import json
+import os
+import shutil
 
 @fapp.route ('/import')
 def import_gps()->bytes:
     return hj.fe._join (hj.fe._static ('/html/import.html'),
                         dev_form=hj.fe.forms.input_device())
+
+@fapp.route ('/import/move', methods=['PUT'])
+def import_move_data()->bytes:
+    xfer_info = json.loads (flask.request.data.decode())
+    device_info = xfer_info['device']
+    dt = hj.device.Type[device_info['type']]
+    extras = hj.fe.forms.extras (dt.name, device_info['extras'])
+    xfer = shutil.move if xfer_info['move'] else shutil.copy
+    with hj.device.open (dt, **extras) as device:
+        for fn in (xfer_info['routes'] +
+                   xfer_info['tracks'] + xfer_info['waypts']):
+            bn = os.path.basename (fn)
+            xfer (fn, os.path.join (hj.config.wdir, bn))
+            pass
+        pass
+    return b''
 
 @fapp.route ('/import/scan', methods=['PUT'])
 def import_scan_device()->bytes:
@@ -28,3 +47,11 @@ def import_scan_device()->bytes:
     return json.dumps ({'routes':routes,
                         'tracks':tracks,
                         'waypts':waypoints}).encode()
+
+@fapp.route ('/import/wipe', methods=['PUT'])
+def import_wipe_device()->bytes:
+    device_info = json.loads (flask.request.data.decode())
+    dt = hj.device.Type[device_info['type']]
+    extras = hj.fe.forms.extras (dt.name, device_info['extras'])
+    with hj.device.open (dt, **extras) as device: hj.device.close (device, True)
+    return b''
--- hj/fe/scripts/import.js
+++ hj/fe/scripts/import.js
@@ -1,7 +1,10 @@
 
-function import_copy (clean)
+function import_copy (clear)
 {
-    var data = { clean:clean, routes:[], tracks:[], waypts:[] };
+    var connection = new XMLHttpRequest();
+    var data = { move:clear, routes:[], tracks:[], waypts:[] };
+    var device = {extras:input_dev_extras(),
+                  type:document.getElementById ("input_device").value};
     var returns = ["routes", "tracks", "waypts"];
     var tables = ["routes", "tracks", "waypoints"];
 
@@ -9,14 +12,45 @@ function import_copy (clean)
     {
         var list = document.getElementById (tables[t]);
 
-        for (o = 0 ; o < list.selectedOptions.length ; o++)
-        { data[returns[t]].push (list.selectedOptions[o].value); }
+        for (o = list.selectedOptions.length - 1; 0 <= o ; o--)
+        {
+            data[returns[t]].push (list.selectedOptions[o].innerHTML);
+            if (clear) { list.removeChild (list.selectedOptions[o]); }
+        }
     }
+    connection.onreadystatechange = function()
+    {
+        if (connection.readyState == 4 && connection.status == 200)
+        {
+            console.log ("done moving data...");
+        }
+    }
+    data.device = device;
+    connection.open("PUT", "/import/move", true);
+    connection.send(JSON.stringify (data));
 }
 
 function import_init()
 {
     input_dev_init();
+}
+
+function import_wipe()
+{
+    console.log ("Wipe the device...");
+    var connection = new XMLHttpRequest();
+    var device = {extras:input_dev_extras(),
+                  type:document.getElementById ("input_device").value};
+
+    connection.onreadystatechange = function()
+    {
+        if (connection.readyState == 4 && connection.status == 200)
+        {
+            console.log ("done wiping data...");
+        }
+    }
+    connection.open("PUT", "/import/wipe", true);
+    connection.send(JSON.stringify (device));
 }
 
 function input_dev_ready()
@@ -39,9 +73,10 @@ function input_dev_ready()
                 var opts = "";
                 
                 for (o = 0 ; o < data[args[t]].length ; o++)
-                {opts += "<option selected value=" + data[args[t]][o] + ">" + data[args[t]][o] + "</option>";}
+                {opts += "<option selected>" + data[args[t]][o] + "</option>";}
             list.innerHTML = opts;
             }
+            console.log ('scan');
         }
     }
     connection.open("PUT", "/import/scan", true);
