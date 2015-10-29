1cf9b166641206a9a3aad2fff8181dd9d96dbdea
Al Niessner <Al.Niessner@gmx.net>
2015-10-29 08:58:44 -0700

    a rough skeleton view for attaching metadata to the GPS information

--- hj/fe/js.py
+++ hj/fe/js.py
@@ -5,8 +5,15 @@ from hj.fe import fapp
 
 import hj.fe
 
+@fapp.route ('/scripts/gpsi.js')
+def gpsi_js() -> bytes: return hj.fe._static ('/scripts/gpsi.js')
+
 @fapp.route ('/scripts/import.js')
 def import_js() -> bytes: return hj.fe._static ('/scripts/import.js')
 
 @fapp.route ('/scripts/input_dev.js')
 def input_dev() -> bytes: return hj.fe._static ('/scripts/input_dev.js')
+
+@fapp.route ('/scripts/metadata.js')
+def metadata_js() -> bytes: return hj.fe._static ('/scripts/metadata.js')
+
