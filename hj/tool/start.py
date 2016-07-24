ae5cb5e5a96da44a2de0ce258c0c27dfff715553
Al Niessner <Al.Niessner@gmx.net>
2016-07-24 13:51:29 -0700

    some minor fixes from previous refactoring

--- hj/tool/start.py
+++ hj/tool/start.py
@@ -17,9 +17,10 @@ if __name__ == '__main__':
     sys.path.append (os.path.abspath (os.path.join (os.path.dirname (__file__),
                                                     '../..')))
     import hj.tool.start
-
+    import hj.util.args
+    
     ap = argparse.ArgumentParser(description='Start the hiking journal. The journal uses the browser as its rendering engine. It uses the twisted platform to tie browswer actions back into modifying the journal data itself.')
-    hj.util.args.base (ap. '${HOME}/.hj.log')
+    hj.util.args.base (ap, '${HOME}/.hj.log')
     ap.add_argument ('-p', '--port', default=8080, type=int,
                      help='the socket port number for browser <-> journal communications [%(default)s]')
     args = ap.parse_args()
